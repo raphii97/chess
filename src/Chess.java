bbf87ac4caeb0423475a52082d600b06cfbb2111
Raphaël Monteiro <monterap@students.zhaw.ch>
2023-08-06 08:43:02 +0200

    added piece move logic

--- src/Chess.java
+++ src/Chess.java
@@ -27,26 +27,41 @@ public class Chess{
         input = scanner.nextLine();
 
         while (!validateInput(input)){
-            System.out.println("Invalid input. Please try again: e.g. a1a2");
+            System.out.println("Invalid input. Please try again: e.g. a2a3");
             input = scanner.nextLine();
         }
 
         return input;
     }
 
+    //TODO: and check if the move is valid
     private boolean validateInput(String input){
         return input.matches("[a-h][1-8][a-h][1-8]");
     }
 
     private void run(){
         String input = "";
+        int x1, y1, x2, y2;
 
-        while (!isCheckmate){
-            input = getInput();
-            move(LETTERS.indexOf(input.charAt(0)), 8 - Character.getNumericValue(input.charAt(1)), LETTERS.indexOf(input.charAt(2)), 8 - Character.getNumericValue(input.charAt(3)));
+        input = getInput();
+
+        x1 = LETTERS.indexOf(input.charAt(0));
+        y1 = 8 - Character.getNumericValue(input.charAt(1));
+        x2 = LETTERS.indexOf(input.charAt(2));
+        y2 = 8 - Character.getNumericValue(input.charAt(3));
+
+        Piece currentPiece = board.getSpot(x1, y1).getPiece();
+
+        if (currentPiece.isMoveValid(x1, 8 - y1, x2, 8 - y2) && currentPiece.getPlayer() == currentPlayer) {
+            move(x1, y1, x2, y2);
             board.printBoard();
             switchPlayer();
         }
+        else {
+            System.out.println("Invalid move. Please try again.");
+        }
+
+        if (!isCheckmate) run();
     }
 
     private void move(int x1, int y1, int x2, int y2){
--- src/Piece.java
+++ src/Piece.java
@@ -7,6 +7,65 @@ public class Piece {
         this.type = type;
     }
 
+    public boolean isMoveValid(int x1, int y1, int x2, int y2){
+        return switch (this.type) {
+            case PAWN -> isPawnMoveValid(x1, y1, x2, y2);
+            case ROOK -> isRookMoveValid(x1, y1, x2, y2);
+            case KNIGHT -> isKnightMoveValid(x1, y1, x2, y2);
+            case BISHOP -> isBishopMoveValid(x1, y1, x2, y2);
+            case QUEEN -> isQueenMoveValid(x1, y1, x2, y2);
+            case KING -> isKingMoveValid(x1, y1, x2, y2);
+        };
+    }
+
+    private boolean isKingMoveValid(int x1, int y1, int x2, int y2) {
+        if (Math.abs(x2 - x1) <= 1 && Math.abs(y2 - y1) <= 1)
+            return true;
+        else
+            return false;
+    }
+
+    private boolean isQueenMoveValid(int x1, int y1, int x2, int y2) {
+        if (isBishopMoveValid(x1, y1, x2, y2) || isRookMoveValid(x1, y1, x2, y2))
+            return true;
+        else
+            return false;
+    }
+
+    private boolean isBishopMoveValid(int x1, int y1, int x2, int y2) {
+        if (Math.abs(x2 - x1) == Math.abs(y2 - y1))
+            return true;
+        else
+            return false;
+    }
+
+    private boolean isKnightMoveValid(int x1, int y1, int x2, int y2) {
+        if (Math.abs(x2 - x1) == 2 && Math.abs(y2 - y1) == 1)
+            return true;
+        else if (Math.abs(x2 - x1) == 1 && Math.abs(y2 - y1) == 2)
+            return true;
+        else
+            return false;
+    }
+
+    private boolean isRookMoveValid(int x1, int y1, int x2, int y2) {
+        if (x1 == x2 && y1 != y2)
+            return true;
+        else if (x1 != x2 && y1 == y2)
+            return true;
+        else
+            return false;
+    }
+
+    private boolean isPawnMoveValid(int x1, int y1, int x2, int y2) {
+        if (x2 - x1 == 0 && y2 - y1 == 2)
+            return true;
+        else if (x2 - x1 == 0 && y2 - y1 == 2 && y1 == 1)
+            return true;
+        else
+            return false;
+    }
+
     public Player getPlayer(){
         return this.player;
     }
